6cbb1e033f7ecc69b8c5dcbb8a9e1df685212c34
maiconkkl <ma_bonitao@hotmail.com>
2020-01-06 20:44:12 -0300

    Correcting and improving translation

--- locales/br.lua
+++ locales/br.lua
@@ -7,5 +7,5 @@ Locales['br'] = {
   ['tattoos']            = 'tatuagens',
   ['tattoo_shop']        = 'loja de tatuagens',
   ['bought_tattoo']      = 'você comprou uma ~y~tatuagem~s~ por ~r~$%s~s~',
-  ['not_enough_money']   = 'você não tem ~r~dinheiro suficiente~s~ para aquela tatuagem! Você está faltando ~r~$%s~s~'
+  ['not_enough_money']   = 'você não tem ~r~dinheiro suficiente~s~ para aquela tatuagem! Está faltando ~r~$%s~s~'
 }
