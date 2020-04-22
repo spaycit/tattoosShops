6772c573b979a7d86d9d1adb62cb2dc8881c7b03
MrLumi <lumilesff@hotmail.com>
2020-04-22 22:12:28 +0100

    Update fr.lua

--- locales/fr.lua
+++ locales/fr.lua
@@ -8,4 +8,24 @@ Locales['fr'] = {
   ['tattoo_shop']        = 'salon de tatouage',
   ['bought_tattoo']      = 'votre nouveau ~y~tatouage~s~ vous a couté ~r~$%s~s~',
   ['not_enough_money']   = 'vous n\'avez pas ~r~assez d\'argent~s~ pour ce tatouage! Il vous manque ~r~$%s~s~'
+  
+  ['business']  = 'Negocios',
+	['hipster']  = 'Hipster',
+	['biker']  = 'Motoqueiro',
+	['airraces']  = 'Corridas aereas',
+	['beach']  = 'Praia',
+	['christmas2']  = 'Natal',
+	['gunrunning']  = 'Tiroteio',
+	['importexport']  = 'Importacao/Exportacao',
+	['lowrider2']  = 'Lowrider2',
+	['lowride']  = 'Lowrider',
+	['luxe']  = 'Luxo',
+	['luxe2']  = 'Luxo 2',
+	['multiplayer']  = 'Multijogador',
+	['vinewood']  = 'Vinewood',
+	['stunt']  = 'Facanha',
+	['smuggler']  = 'Contrabandista',
+	['heist3']  = 'Assalto',
+	['christmas2018']  = 'Natal 2018',
+	['christmas2017']  = 'Natal 2017'
 }
